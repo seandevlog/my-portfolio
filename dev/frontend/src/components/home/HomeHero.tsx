8feeaca2bbbab41acfbee09037584549fd94e986
unknown <seandevlog@proton.me>
2026-06-29 16:09:45 -0400

    Developed Responsive Home Hero Section

--- dev/frontend/src/components/home/HomeHero.tsx
+++ dev/frontend/src/components/home/HomeHero.tsx
@@ -6,11 +6,11 @@ export default function HomeHero() {
   return (
     <div className="h-dvh w-full flex flex-col items-center justify-center gap-[24px] px-section-x-xs">
       <Status color="bg-[#10b981]">system_status: active</Status>
-      <span className="w-full max-w-[360px] font-outfit text-h1-xs justify-center text-center uppercase">
+      <span className="w-full s:w-[550px] m:w-[768px] l:w-[959px] max-w-[360px] s:max-w-none font-outfit text-h1-xs s:text-h1-s m:text-h1-m l:text-h1-l justify-center text-center uppercase">
         <p className="inline-flex items-center gap-2  text-secondary-lighter">backend <X/> data</p>
         <p className="text-secondary-lightest">for secure, structured systems</p>
       </span>
-      <span className="w-full max-w-[320px] font-outfit text-p1-xs text-center text-pretty text-secondary-lightest">
+      <span className="w-full s:w-[530px] m:w-[700px] xl:w-[800px] max-w-[320px] s:max-w-none font-outfit text-p1-xs s:text-p1-s m:text-p1-m l:text-p1-l xl:text-p1-xl text-center text-pretty text-secondary-lightest">
         <p>I turn practical ideas into secure web platforms, connecting clean APIs, reliable data flows, and thoughtful interfaces into systems people can trust.</p>
       </span>
       <ButtonPrimary>view_systems</ButtonPrimary>
--- dev/frontend/src/components/ui/ButtonPrimary.tsx
+++ dev/frontend/src/components/ui/ButtonPrimary.tsx
@@ -3,11 +3,11 @@ import { Button } from "@mantine/core";
 export default function ButtonPrimary({ children }: { children: string }) {
   return (
     <Button
-
       radius="xs"
       classNames={{
-        root: "px-[20px] py-[18px] bg-secondary-base",
-        label: "uppercase font-jetbrains text-[12px] text-secondary-dark"
+        root: "!h-auto !min-h-0 !py-[18px] !px-[20px] bg-secondary-base",
+        inner: "!h-auto",
+        label: "uppercase font-jetbrains text-button-xs m:text-button-m l:text-button-l xl:text-button-xl text-secondary-dark"
       }}
     >
       {children}
