d5f9925e713f9f7e1d47d23461d5707ea713cce4
unknown <seandevlog@proton.me>
2026-07-09 14:59:25 -0400

    Added button functionality to scroll to Projects Section

--- dev/frontend/src/components/home/HomeHero.tsx
+++ dev/frontend/src/components/home/HomeHero.tsx
@@ -4,16 +4,32 @@ import Button from "../ui/Button";
 
 export default function HomeHero() {
   return (
-    <section className="h-dvh w-full flex flex-col items-center justify-center gap-[24px] px-section-x-xs">
+    <section className="flex h-dvh w-full flex-col items-center justify-center gap-[24px] px-section-x-xs">
       <Status color="green">system_status: active</Status>
-      <span className="w-full s:w-[550px] m:w-[768px] l:w-[959px] max-w-[360px] s:max-w-none text-h1-xs s:text-h1-s m:text-h1-m l:text-h1-l justify-center text-center uppercase">
-        <p className="inline-flex items-center gap-2  text-secondary-lighter">backend <X className="w-[20px] h-[20px] s:w-[28px] s:h-[28px] m:w-[48px] m:h-[48px]"/> data</p>
-        <p className="text-secondary-lightest">for secure, structured systems</p>
+
+      <span className="w-full max-w-[360px] justify-center text-center text-h1-xs uppercase s:w-[550px] s:max-w-none s:text-h1-s m:w-[768px] m:text-h1-m l:w-[959px] l:text-h1-l">
+        <p className="inline-flex items-center gap-2 text-secondary-lighter">
+          backend{" "}
+          <X className="h-[20px] w-[20px] s:h-[28px] s:w-[28px] m:h-[48px] m:w-[48px]" />{" "}
+          data
+        </p>
+
+        <p className="text-secondary-lightest">
+          for secure, structured systems
+        </p>
       </span>
-      <span className="w-full s:w-[530px] m:w-[700px] xl:w-[800px] max-w-[320px] s:max-w-none text-p1-xs s:text-p1-s m:text-p1-m l:text-p1-l xl:text-p1-xl text-center text-pretty text-secondary-lightest">
-        <p>I turn practical ideas into secure web platforms, connecting clean APIs, reliable data flows, and thoughtful interfaces into systems people can trust.</p>
+
+      <span className="w-full max-w-[320px] text-pretty text-center text-p1-xs text-secondary-lightest s:w-[530px] s:max-w-none s:text-p1-s m:w-[700px] m:text-p1-m l:text-p1-l xl:w-[800px] xl:text-p1-xl">
+        <p>
+          I turn practical ideas into secure web platforms, connecting clean
+          APIs, reliable data flows, and thoughtful interfaces into systems
+          people can trust.
+        </p>
       </span>
-      <Button mode="primary">view_systems</Button>
+
+      <a href="#projects" aria-label="Scroll to projects section">
+        <Button mode="primary">view_systems</Button>
+      </a>
     </section>
-  )
+  );
 }
--- dev/frontend/src/components/home/HomeProjects.tsx
+++ dev/frontend/src/components/home/HomeProjects.tsx
@@ -4,7 +4,10 @@ import ProjectsShowcase from "../ui/ProjectsShowcase";
 
 export default function HomeProjects() {
   return (
-    <section className="relative flex h-fit w-full justify-center px-section-x-xs py-section-y-xs s:px-section-x-s s:py-section-y-s m:min-h-dvh xl:min-h-0 m:px-section-x-m m:py-section-y-m l:px-section-x-l l:py-section-y-l xl:py-section-y-xl">
+    <section
+      id="projects" 
+      className="relative flex h-fit w-full scroll-mt-[90px] justify-center px-section-x-xs py-section-y-xs s:px-section-x-s s:py-section-y-s m:min-h-dvh xl:min-h-0 m:px-section-x-m m:py-section-y-m l:px-section-x-l l:py-section-y-l xl:py-section-y-xl"
+    >
       <div className="flex h-fit w-full min-w-section-minw-xs flex-col gap-[40px] s:min-w-section-minw-s m:min-w-section-minw-m l:min-w-section-minw-l">
         <div className="flex h-fit w-full flex-col gap-[12px]">
           <Status color="orange">system_status: projects</Status>
